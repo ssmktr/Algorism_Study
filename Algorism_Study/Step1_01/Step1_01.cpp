85d646368939dca45c3bb7170c46d10d9f8798f3
ssmktr87 <ssmktr@gmail.com>
2020-04-22 20:40:55 +0900

    no message

--- Algorism_Study/Step1_01/Step1_01.cpp
+++ Algorism_Study/Step1_01/Step1_01.cpp
@@ -1,6 +1,33 @@
 ﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main()
 {
+	const int MAX_SIZE = 10000000;
+	vector<int> vec;
+	int n;
+	cin >> n;
+	if (n < 1 || n > MAX_SIZE)
+		return 0;
+
+	vec.resize(n + 1);
+	int size = (int)vec.size();
+	for (int i = 0; i < size; ++i)
+		vec[i] = 0;
+
+	for (int i = 2; i <= n; ++i)
+	{
+		vec[i] = vec[i - 1] + 1;
+		if (i % 2 == 0)
+			vec[i] = min(vec[i], vec[i / 2] + 1);
+		if (i % 3 == 0)
+			vec[i] = min(vec[i], vec[i / 3] + 1);
+	}
+
+	cout << vec[n] << endl;
+
 	return 0;
 }
---
+++ Algorism_Study/Step1_02/Step1_02.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
