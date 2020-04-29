60444d3cae32a918d67e7bef97621044b945b1f6
ssmktr87 <ssmktr@gmail.com>
2020-04-29 21:17:21 +0900

    no message

--- Algorism_Study/Step001/Question02/Question02.cpp
+++ Algorism_Study/Step001/Question02/Question02.cpp
@@ -7,12 +7,23 @@ vector<int> solution(int n)
 {
 	vector<int> answer;
 
+	for (int i = 0; i < n; ++i)
+	{
+		answer.push_back(0);
+
+		vector<int> temp = answer;
+		for (int j = temp.size() - 2; j >= 0; --j)
+		{
+			answer.push_back((temp[j] == 1 ? 0 : 1));
+		}
+	}
+
 	return answer;
 }
 
 int main()
 {
-	vector<int> result = solution(2);
+	vector<int> result = solution(3);
 
 	for (int i = 0; i < result.size(); ++i)
 		cout << result[i] << ", ";
