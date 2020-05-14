773161195d8ec7d073b0b1b9548f15cc8156b989
ssmktr87 <ssmktr@gmail.com>
2020-05-14 15:56:04 +0900

    no message

---
+++ Algorism_Study/Step002/Question02_01/Question02_01.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees) {
+	int answer = 0;
+	int tf = 1;
+	int check = 0;
+	for (int i = 0; i < skill_trees.size(); i++) {
+		string vec_str = skill_trees[i];
+		for (int j = 0; j < vec_str.size(); j++) {
+			// vec_str[j]와 find_num 비교하기
+			int find_num = skill.find(vec_str[j]);
+			// 범위에맞지않을 경우 
+			if (find_num < 0 || find_num > 30) {
+				continue;
+			}
+			// 체크된 개수랑 find()한 개수가 틀릴경우 
+			if (find_num != check) {
+				tf = 0;
+				break;
+			}
+			//동일한 문자가 있을경우 통과하면
+			else {
+				check++;
+			}
+		}
+		if (tf) {
+			answer++;
+		}
+		check = 0;
+		tf = 1;
+	}
+	return answer;
+}
+
+int main()
+{
+	vector<string> skill_trees = { "BACDE", "CBADF", "AECB", "BDA" };
+	int result = solution("CBD", skill_trees);
+	cout << result << endl;
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
