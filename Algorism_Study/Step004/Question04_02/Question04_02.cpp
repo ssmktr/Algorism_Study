2219ff6094b5f07da6a98ea6716cb5608cbc3395
ssmktr87 <ssmktr@gmail.com>
2020-05-29 18:52:44 +0900

    no message

--- Algorism_Study/Step004/Question04_02/Question04_02.cpp
+++ Algorism_Study/Step004/Question04_02/Question04_02.cpp
@@ -1,12 +1,30 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(int n, vector<vector<int>> costs) {
 	int answer = 0;
 
+	int max = 0;
+	for (int i = 0; i < costs.size(); ++i)
+	{
+		int value = costs[i][1];
+		if (value > max)
+			max = value;
+	}
+	vector<int> parent;
+	for (int i = 0; i <= max; ++i)
+		parent.push_back(i);
+
+	//sort(costs.begin(), costs.end(), [=](vector<int> comp1, vector<int> comp2) -> bool 
+	//{
+	//	return comp1[2] < comp2[2];
+	//});
+
+	vector<int> parent;
 
 
 	return answer;
