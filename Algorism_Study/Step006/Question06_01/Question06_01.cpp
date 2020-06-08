b245bb51ce9894ac8de6fb1b6aab13902f2055da
ssmktr87 <ssmktr@gmail.com>
2020-06-08 18:48:32 +0900

    no message

---
+++ Algorism_Study/Step006/Question06_01/Question06_01.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+	int answer = priorities.size() - 1;
+
+	vector<int> check;
+
+	int idx = 0;
+	while (priorities.size() != 0)
+	{
+		int value = priorities[idx];
+		for (int i = idx; i < priorities.size(); ++i)
+		{
+			if (priorities[i] > value)
+				idx = i;
+		}
+
+		check.push_back(priorities[idx]);
+		priorities.erase(priorities.begin() + idx);
+
+		idx--;
+		if (idx >= priorities.size())
+			idx = 0;
+	}
+
+	for (int i = 0; i < check.size(); ++i)
+	{
+		if (check[i] == location)
+		{
+			answer = i;
+			break;
+		}
+	}
+
+	return answer;
+}
+
+int main()
+{
+	vector<int> priorities = { 2,1,3,2 };
+	int location = 2;
+	int result = solution(priorities, location);
+	cout << result << endl;
+
+	priorities = { 1, 1, 9, 1, 1, 1 };
+	location = 0;
+	result = solution(priorities, location);
+	cout << result << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ Algorism_Study/Step006/Question06_02/Question06_02.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+	int answer = 0;
+
+	const int MAX_SIZE = 1000;
+	if (n < 1 || n > MAX_SIZE)
+		return 0;
+
+	vector<int> vec;
+	vec.resize(n + 1);
+
+	vec[0] = 1;
+	vec[1] = 1;
+
+	int size = (int)vec.size();
+	for (int i = 2; i < size; ++i)
+	{
+		vec[i] = (vec[i - 1] + (vec[i - 2] * 2)) % 10007;
+	}
+
+	return answer;
+}
+
+int main()
+{
+	//int n = 4;
+	//int result = solution(n);
+	//cout << result << endl;
+
+	const int MAX_SIZE = 1000;
+	int n = 0;
+	cin >> n;
+
+	if (n < 1 || n > MAX_SIZE)
+		return 0;
+
+	vector<int> vec;
+	vec.resize(n + 1);
+
+	vec[0] = 1;
+	vec[1] = 1;
+
+	int size = (int)vec.size();
+	for (int i = 2; i < size; ++i)
+	{
+		vec[i] = (vec[i - 1] + (vec[i - 2] * 2)) % 10007;
+	}
+
+	cout << vec[n] << endl;
+
+	system("pause");
+	return 0;
+}
