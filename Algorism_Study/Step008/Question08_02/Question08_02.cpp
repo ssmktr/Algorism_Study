690e9b7660dc66924d73e347db072a4aebac36a5
ssmktr87 <ssmktr@gmail.com>
2020-07-01 17:06:56 +0900

    no message

---
+++ Algorism_Study/Step008/Question08_02/Question08_02.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> routes) {
+	int answer = 0;
+	return answer;
+}
+
+int main()
+{
+	vector<vector<int>> routes = {
+		{-20, 15},
+		{-14, -5},
+		{-18, -13},
+		{-5, -3}
+	};
+	int result = solution(routes);
+	cout << result << endl;
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
